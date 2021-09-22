e0b836b38c640e7b78cbe3af944f1b238ef29376
yokota chiune <yokota.chiune@gmail.com>
2021-09-22 23:40:22 +0900

    ヘッダーデザイン　ハンバーガーメニュー実装

--- src/components/atoms/layouts/Header.jsx
+++ src/components/atoms/layouts/Header.jsx
@@ -1,23 +1,47 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Header = (props) => {
+  const [ showMenu, setShowMenu ] = useState(false);
+  const onClickHumburger = () => {
+    showMenu === true ? setShowMenu(false) : setShowMenu(true);
+  }
   return (
     <>
       <SHeader>
-        <SLink to="/">Top</SLink><br />
-        <SLink to="/about">About</SLink><br />
-        <SLink to="/carrer">History</SLink>
-        <SLink to="/personality">Personality</SLink>
+        {showMenu ? (
+          <SNav>
+            <SUl>
+              <SLink to="/">Top</SLink>
+              <SLink to="/about">About</SLink>
+              <SLink to="/carrer">History</SLink>
+              <SLink to="/personality">Personality</SLink>
+            </SUl>
+          </SNav>
+        ) : (
+        <></>
+        )}
+        <SHamburger onClick={onClickHumburger}>
+          <SLine1></SLine1>
+          <SLine2></SLine2>
+          <SLine3></SLine3>
+        </SHamburger>
       </SHeader>
     </>
   );
 }
 
-const SHeader = styled.header`
-  height: 60px;
+const SNav = styled.nav`
+  height: 66px;
+  position: absolute;
+  top: 90px;
   width: 100%;
   background: #000;
+`
+
+const SUl = styled.ul`
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -25,9 +49,55 @@ const SHeader = styled.header`
 
 const SLink = styled(Link)`
   color: #bea76f;
-  margin: 8px;
+  padding: 20px 25px;
   text-decoration: none;
   &:hover{
     cursor: pointer;
   }
+`
+
+const SHamburger = styled.div`
+  display: block;
+  position: absolute;
+  right: 20px;
+  width: 17px;
+  height: 44px;
+  cursor: pointer;
+  transition: 1s;
+`
+
+const SLines = styled.span`
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+
+  width: 25px;
+  height: 3px;
+  background: #bea76f;
+  transition: 1s;
+  border-radius: 4px;
+`
+
+const SLine1 = styled(SLines)`
+  top: 35%;
+`
+const SLine2 = styled(SLines)`
+`
+const SLine3 = styled(SLines)`
+  top: 65%;
+`
+
+const SHeader = styled.header`
+  height: 90px;
+  width: 100%;
+  background: #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
 `
