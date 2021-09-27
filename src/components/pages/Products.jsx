8b7d4404f3451438fd34e496c5123ccc40467d60
yokota chiune <yokota.chiune@gmail.com>
2021-09-27 22:50:02 +0900

    Productの詳細をMaskに追加

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import backgroundPc from './img/background-pc.jpg'
 
 const products = [
   {
+    url: "/",
     title: "Portfolio",
-    comment: "このサイトです。",
-    skills: "HTML5"
+    comment: "Reactのアウトプットとして作成しました。",
+    skills: "HTML5, CSS3, JavaScript, React, styled-components",
+    other: "Atomic Design, レスポンシブ対応",
   },
   {
+    url: "#",
     title: "Comming soon...",
-    comment: "制作中",
+    comment: "Comming soon...",
   },
 ]
 
@@ -27,7 +31,19 @@ export const Products = () => {
                 <SProduct>
                   <SImg src={backgroundPc} alt="{e.title}" />
                   <SProductTitle>{e.title}</SProductTitle>
-                  <SProductComment>{e.comment}</SProductComment>
+                  <SMask to={e.url}>
+                      { e.skills ? (
+                        <>
+                    <SMaskText>
+                          <SProductComment>{e.comment}</SProductComment>
+                          <SProductComment>使用技術：{e.skills}</SProductComment>
+                          <SProductComment>その他：{e.other}</SProductComment>
+                    </SMaskText>
+                        </>
+                        ):(
+                          <SMakingComment>{e.comment}</SMakingComment>
+                        ) }
+                  </SMask>
                 </SProduct>
               </>
             );
@@ -38,9 +54,29 @@ export const Products = () => {
     </>
   );
 }
-// モーダルで
-// 左側拡大写真
-// 右側データ => 使用技術とか、簡単なreadme的なものをかけると良い
+
+const SMaskText = styled.div`
+  padding: 0 0 20px 0;
+`
+
+const SMask = styled(Link)`
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	opacity: 0;
+	background-color:	rgba(0,0,0,0.4);
+  color: #fff;
+  text-decoration: none;
+
+  display: flex;
+  align-items: flex-end;
+
+  &:hover {
+    opacity: .5;
+  }
+`
 const SContainer = styled.div`
   min-height: 100vh;
   padding: 20px 0 0 0;
@@ -60,9 +96,10 @@ const SProducts = styled.div`
 const SProduct = styled.div`
   margin: 0 20px 70px;
   max-width: 400px;
-  :last-child {
+  position:		relative;
+  /* :last-child {
     pointer-events: none;
-  }
+  } */
 `
 const SProductTitle = styled.h3`
   font-family: 'Noto Serif',serif;
@@ -73,20 +110,31 @@ const SProductTitle = styled.h3`
   margin: 10px 0 0 0;
 `
 const SProductComment = styled.p`
-  margin: 5px 0 0 0;
+  width: 90%;
+  margin: 5px auto 0 auto;
   font-family: "Noto Serif",serif;
   font-style: normal;
-  font-size: 12px;
+  font-size: 14px;
   line-height: 1.2;
   font-weight: 400;
-  text-align: center;
+`
+const SMakingComment = styled.p`
+  width: 100%;
+  height: 100%;
+  font-family: "Noto Serif",serif;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1.2;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 const SImg = styled.img`
   width: 100%;
   height: 100%;
   transition: .2s;
   &:hover {
-    opacity: .5;
     cursor: pointer;
   }
 `
